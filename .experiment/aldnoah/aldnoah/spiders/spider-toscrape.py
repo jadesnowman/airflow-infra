ca36f8d3ea3b2721b5e22a2d5247369ace040027
ilhamsj <ilhamsaputrajati@gmail.com>
2025-03-01 15:57:53 +0700

    Add initial Scrapy project structure with spiders, middlewares, pipelines, and settings

---
+++ .experiment/aldnoah/aldnoah/spiders/spider-toscrape.py
@@ -0,0 +1,19 @@
+
+import scrapy
+
+class MySpider(scrapy.Spider):
+    name = "my_spider"
+    start_urls = ["http://quotes.toscrape.com/"]
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                "text": quote.css("span.text::text").get(),
+                "author": quote.css("small.author::text").get(),
+                "tags": quote.css("div.tags a.tag::text").getall(),
+            }
+
+        # Follow pagination links
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page:
+            yield response.follow(next_page, self.parse)
